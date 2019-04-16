93ecf096032764b0a2f6d18a9edde352176feb9b
d-mv <d.dash.mv@gmail.com>
2019-04-16 13:12:27 +0300

    Updated README, updated package.json, build

---
+++ build/precache-manifest.00777369851a18bc412478cbe4b44d9b.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "3a5e62b986cbc2e8f71e",
+    "url": "/home-react-styled/build/static/js/main.86c9ee51.chunk.js"
+  },
+  {
+    "revision": "f3d955e53c56d81b39d3",
+    "url": "/home-react-styled/build/static/js/runtime~main.5d3fd8d3.js"
+  },
+  {
+    "revision": "32236657274cba512ef4",
+    "url": "/home-react-styled/build/static/js/2.2599e73a.chunk.js"
+  },
+  {
+    "revision": "704a398061d8175865c3c1e4544ea814",
+    "url": "/home-react-styled/build/index.html"
+  }
+];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/home-react-styled/build/precache-manifest.bb2f3cf0da30b72ed5f26104a6298fb1.js"
+  "/home-react-styled/build/precache-manifest.00777369851a18bc412478cbe4b44d9b.js"
 );
 
 workbox.clientsClaim();
