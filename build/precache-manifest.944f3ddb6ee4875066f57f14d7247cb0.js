549bf00a9833d7fe9abe17ec7c6ea05236e450a2
d-mv <d.dash.mv@gmail.com>
2019-07-31 21:31:29 +0300

    build

---
+++ build/precache-manifest.944f3ddb6ee4875066f57f14d7247cb0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5917a4b0c18dd539ce14f4a2a249227c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "382c412153f6ea8c0c53",
+    "url": "/static/css/main.7a6655bb.chunk.css"
+  },
+  {
+    "revision": "05cc873bcf8814ddbb7f",
+    "url": "/static/js/2.5f46c4b1.chunk.js"
+  },
+  {
+    "revision": "382c412153f6ea8c0c53",
+    "url": "/static/js/main.6dc9f380.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
---
+++ src/style/Navigation.ts
@@ -0,0 +1,5 @@
+import styled from 'styled-components'
+
+const Style = styled.aside``
+
+export default Style
